62be949f43fcb8419dd09b7ddf566755da98ab3a
shadowghost94 <salifououzerou25@gmail.com>
2023-09-03 23:07:14 +0100

    premier dépôt

---
+++ listeDoublement.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+typedef struct Node Node;
+struct Node
+{
+    int val;
+    Node* succ;
+    Node* pred;
+};
+
+typedef struct Linkedlist Linkedlist;
+struct Linkedlist
+{
+
+};
+
+int main()
+{
+    return 0;
+}
